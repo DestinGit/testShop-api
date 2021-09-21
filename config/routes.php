5ba97ea6fbc2069ebe658b54168531d227790c15
Destin <d.gandobak@gmail.com>
2021-09-21 14:45:11 +0200

    correction refactor des routes

--- config/routes.php
+++ config/routes.php
@@ -9,17 +9,20 @@ use Slim\Routing\RouteCollectorProxy;
 use Tuupola\Middleware\JwtAuthentication;
 
 return function (App $app) {
-	$app->post('/auth/login', [AuthController::class, 'signIn'])->add(new ValidationErrorsMiddleware());
-	$app->post('/auth/signup', [AuthController::class, 'signUp'])
-		->add(new \App\Middleware\UserWithThatEmailAlreadyExistsMiddleware())
-		->add(new ValidationErrorsMiddleware());
 	$app->get('/categories', [CategoryController::class, 'getAll']);
+
+	$app->group('/auth', function (RouteCollectorProxy $group) {
+		$group->post('/login', [AuthController::class, 'signIn']);
+		$group->post('/signup', [AuthController::class, 'signUp'])
+			->add(new \App\Middleware\UserWithThatEmailAlreadyExistsMiddleware());
+	})->add(new ValidationErrorsMiddleware());
 
 	$app->group('/user', function (RouteCollectorProxy $group) {
 		$group->get('/{id:[0-9]+}', [UserController::class, 'get']);
 		$group->get('/all', [UserController::class, 'getAll']);
 		$group->get('/logout', [AuthController::class, 'signOut']);
 	})
-		->addMiddleware($app->getContainer()->get(JwtAuthentication::class));
+		->add($app->getContainer()->get(JwtAuthentication::class));
+//		->addMiddleware($app->getContainer()->get(JwtAuthentication::class));
 //		->add(new \App\Middleware\AuthMiddleware());
 };
